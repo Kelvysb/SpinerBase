1e658fcab62e9a3636e92ee885ba8a14066d9b96
Kelvys Boniek Pantaleão <contato@kelvysb.com>
2018-09-16 22:56:52 -0300

    Added licence information.

--- SpinerBaseBETests/Layers/BackEnd/SpinerBaseBOTests.cs
+++ SpinerBaseBETests/Layers/BackEnd/SpinerBaseBOTests.cs
@@ -1,4 +1,41 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
+/*
+Copyright 2018 Kelvys B. Pantaleão
+
+This file is part of SpinerBase
+
+SpinerBase Is free software: you can redistribute it And/Or modify
+it under the terms Of the GNU General Public License As published by
+the Free Software Foundation, either version 3 Of the License, Or
+(at your option) any later version.
+
+This program Is distributed In the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty Of
+MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License For more details.
+
+You should have received a copy Of the GNU General Public License
+along with this program.  If Not, see <http://www.gnu.org/licenses/>.
+
+Este arquivo é parte Do programa SpinerBase
+
+SpinerBase é um software livre; você pode redistribuí-lo e/ou 
+modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+publicada pela Fundação Do Software Livre (FSF); na versão 3 da 
+Licença, ou(a seu critério) qualquer versão posterior.
+
+Este programa é distribuído na esperança de que possa ser  útil, 
+mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+Licença Pública Geral GNU para maiores detalhes.
+
+Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+com este programa, Se não, veja <http://www.gnu.org/licenses/>.
+
+'GitHub: https://github.com/Kelvysb/SpinerBase
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SpinerBase.Basic;
 using SpinerBase.Layers.BackEnd;
 using System;
@@ -156,10 +193,10 @@ namespace SpinerBase.Layers.BackEnd.Tests
                 objCard.DataBaseType = enmDataBaseType.SQLite;
                 objCard.Name = "Test Card";
                 objCard.Description = "Test Card";
-                objCard.Command = "Select * from testTable where age > #AGE;;";
+                objCard.Command = "Select * from testTable where age > <%AGE%>;;";
                 objCard.Parameters.Add(new Parameter());
                 objCard.Parameters.Last().Description = "Age";
-                objCard.Parameters.Last().Tag = "#AGE;";
+                objCard.Parameters.Last().Tag = "<%AGE%>";
                 objCard.Parameters.Last().Value = "20";
 
                 return objCard;
@@ -182,10 +219,10 @@ namespace SpinerBase.Layers.BackEnd.Tests
                 objCard.DataBaseType = enmDataBaseType.SQLite;
                 objCard.Name = "Test Card";
                 objCard.Description = "Test Card";
-                objCard.Command = "Select Name || ' ' || Surname || ' Died with ' || Age || ' years.' from testTable where Age > #AGE;;";
+                objCard.Command = "Select Name || ' ' || Surname || ' Died with ' || Age || ' years.' from testTable where Age > <%AGE%>;;";
                 objCard.Parameters.Add(new Parameter());
                 objCard.Parameters.Last().Description = "Age";
-                objCard.Parameters.Last().Tag = "#AGE;";
+                objCard.Parameters.Last().Tag = "<%AGE%>";
                 objCard.Parameters.Last().Value = "20";
 
                 return objCard;
